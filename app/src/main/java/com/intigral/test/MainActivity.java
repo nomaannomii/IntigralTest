cebc4d05ac222d93007394eb16db19052b6c91c4
Naveed Rehman <naveed@evento.ae>
2017-10-06 09:36:10 +0400

    Initial commit

---
+++ app/src/main/java/com/intigral/test/MainActivity.java
@@ -0,0 +1,15 @@
+package com.intigral.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.intigral.test.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
